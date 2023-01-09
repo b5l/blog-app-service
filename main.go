0a15f6f19973943343f88c99805026bdffa7d809
Eller Daniel <edanimark@codingsans.com>
2023-01-09 17:36:56 +0100

    blog details routing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	blogDetails "blog-app-service/internal/api/blog_details"
 	blogPosts "blog-app-service/internal/api/blog_posts"
 	"blog-app-service/internal/api/login"
 	"blog-app-service/internal/dal"
@@ -21,11 +22,15 @@ func main() {
 	blogPosts := &blogPosts.Handler{
 		BlogPostsDAO: dal.NewBlogPostsDAO(DB),
 	}
+	blogDetails := &blogDetails.Handler{
+		BlogDetailsDAO: dal.NewBlogDetailsDAO(DB),
+	}
 
 	router := gin.Default()
 	api := router.Group("/api")
 	api.POST("/login", login.LoginHandler)
 	api.GET("/blogPosts", blogPosts.BlogPostsHandler)
+	api.GET("/blogPost/:id", blogDetails.BlogDetailsHandler)
 
 	router.Run("localhost:8080")
 }
