7cfe105486d36883993406597d0181d7527172e8
Eller Daniel <edanimark@codingsans.com>
2023-01-11 16:40:33 +0100

    blog create and edit handler call in main

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	blogCreate "blog-app-service/internal/api/blog_create"
 	blogDetails "blog-app-service/internal/api/blog_details"
+	blogEdit "blog-app-service/internal/api/blog_edit"
 	blogPosts "blog-app-service/internal/api/blog_posts"
 	"blog-app-service/internal/api/login"
 	"blog-app-service/internal/dal"
@@ -25,12 +27,20 @@ func main() {
 	blogDetails := &blogDetails.Handler{
 		BlogDetailsDAO: dal.NewBlogDetailsDAO(DB),
 	}
+	blogCreate := &blogCreate.Handler{
+		BlogCreateEditDAO: dal.NewBlogCreateEditDAO(DB),
+	}
+	blogEdit := &blogEdit.Handler{
+		BlogCreateEditDAO: dal.NewBlogCreateEditDAO(DB),
+	}
 
 	router := gin.Default()
 	api := router.Group("/api")
 	api.POST("/login", login.LoginHandler)
 	api.GET("/blogPosts", blogPosts.BlogPostsHandler)
 	api.GET("/blogPost/:id", blogDetails.BlogDetailsHandler)
+	api.POST("/blogCreate", blogCreate.BlogCreateHandler)
+	api.PUT("/blogEdit", blogEdit.BlogEditHandler)
 
 	router.Run("localhost:8080")
 }
